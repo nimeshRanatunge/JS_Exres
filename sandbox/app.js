3936944bc5354d2cc9c54667cb56f91ec1ff2317
Nimesh Ranatunge <2018cs131@stu.ucsc.cmb.ac.lk>
2022-06-16 00:03:06 +0530

    stt

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,3 +1,39 @@
 const numbers = [1,2,3,4];
 numbers.push(77);
 console.log(numbers);
+
+const car = {
+    name: 'honda',
+    model: 'civic'
+}
+car.year = 1996;
+console.log(car.year);
+
+let v;
+let j;
+v = String(5);
+j = (5).toString();
+console.log(v+5);
+console.log(j+5);
+
+v = Number('gd');
+console.log(v.toFixed(3));
+
+//parseInt, parseFloat
+//math round, floor, ceil
+
+v = 'University';
+console.log(v.charAt(1));
+console.log(v.indexOf('n'));
+
+v = 'University of Colombo';
+console.log(v.split(' ')[0]);
+
+console.log(v.replace('Colombo', 'Colarado'));
+
+v = 'University of Jaffna';
+if(v.includes('Jaffna')){
+    console.log('There are students from Jaffna University');
+}else{
+    console.log('jaffna not participated');
+}
